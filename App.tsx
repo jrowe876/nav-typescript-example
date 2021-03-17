566254d2975e09e4dc533cb8e2d297fbf087925e
jrowe876 <65268302+jrowe876@users.noreply.github.com>
2021-03-17 14:48:04 -0500

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,86 @@
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack';
+
+type RootStackParamList = {
+  Home: undefined,
+  Notifications: undefined,
+  Sun: undefined
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+type HomeScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'Home'
+>;
+
+type SunScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'Sun'
+>;
+
+type NotificationScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'Notifications'
+>;
+
+
+function HomeScreen() {
+  const navigation = useNavigation<HomeScreenNavigationProp>();
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+      <Button
+        onPress={() => navigation.navigate('Sun')}
+        title="Go to sun screen"
+      />
+    </View>
+  );
+}
+
+function SunScreen() {
+  const navigation = useNavigation<SunScreenNavigationProp>();
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Sun Screen</Text>
+      <Button
+        onPress={() => navigation.goBack()}
+        title="Go back home"
+      />
+    </View>
+  );
+}
+
+function NotificationsScreen() {
+  const navigation = useNavigation<NotificationScreenNavigationProp>();
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Notification Screen</Text>
+      <Button
+        onPress={() => navigation.goBack()}
+        title="Go back home"
+      />
+    </View>
+  );
+}
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Notifications" component={NotificationsScreen} />
+        <Stack.Screen name="Sun" component={SunScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 

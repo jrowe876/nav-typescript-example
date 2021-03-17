5915778771924a02c06127f930819f43f3113e08
jrowe876 <65268302+jrowe876@users.noreply.github.com>
2021-03-17 15:03:10 -0500

    Added comments

--- App.tsx
+++ App.tsx
@@ -3,36 +3,45 @@ import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack';
 
+//We create the route type here
 type RootStackParamList = {
   Home: undefined,
   Notifications: undefined,
   Sun: undefined
 };
 
+//Create the Stack object and give it the type we created above
 const Stack = createStackNavigator<RootStackParamList>();
 
+
+//Create the type that we will use for the navigation object
 type HomeScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
   'Home'
 >;
 
+//Create the type that we will use for the navigation object
 type SunScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
   'Sun'
 >;
 
+//Create the type that we will use for the navigation object
 type NotificationScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
   'Notifications'
 >;
 
-
+//instead of HomeScreen({navigation}) we use a slightly approach to accomplish the same thing
 function HomeScreen() {
+
+  //We create the navigation object from scratch and give it the type we created above
   const navigation = useNavigation<HomeScreenNavigationProp>();
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
       <Button
+      //Then here we can use it normally
         onPress={() => navigation.navigate('Notifications')}
         title="Go to notifications"
       />
